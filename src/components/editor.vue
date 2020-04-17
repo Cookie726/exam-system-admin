d2f34ce785ba287ac5938222824508b301c83fb0
Cookie726 <2544967914@qq.com>
2020-04-18 01:20:59 +0800

    功能优化

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -14,22 +14,47 @@ export default {
     return {
       // uploadPath,
       editor: null,
-      info_: null
+      info_: null,
     };
   },
   model: {
     prop: "value",
-    event: "change"
+    event: "change",
   },
   props: {
     value: {
       type: String,
-      default: ""
+      default: "",
     },
     isClear: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
+    configMenus: {
+      default: () => {
+        return [
+          "bold", // 粗体
+          "italic", // 斜体
+          "underline", // 下划线
+          "fontSize", // 字号
+          "fontName", // 字体
+          "strikeThrough", // 删除线
+          "foreColor", // 文字颜色
+          "backColor", // 背景颜色
+          "link", // 插入链接
+          "list", // 列表
+          "justify", // 对齐方式
+          "quote", // 引用
+          "emoticon", // 表情
+          "image", // 插入图片
+          "table", // 表格
+          "video", // 插入视频
+          "code", // 插入代码
+          "fullscreen", // 全屏
+        ];
+      },
+      type: Array,
+    },
   },
   watch: {
     isClear(val) {
@@ -43,7 +68,7 @@ export default {
       if (value !== this.editor.txt.html()) {
         this.editor.txt.html(this.value);
       }
-    }
+    },
     //value为编辑框输入的内容，这里我监听了一下值，当父组件调用得时候，如果给value赋值了，子组件将会显示父组件赋给的值
   },
   mounted() {
@@ -60,33 +85,14 @@ export default {
       this.editor.customConfig.uploadImgParams = {
         // 如果版本 <=v3.1.0 ，属性值会自动进行 encode ，此处无需 encode
         // 如果版本 >=v3.1.1 ，属性值不会自动 encode ，如有需要自己手动 encode
-        file_type: "img"
+        file_type: "img",
       };
       this.editor.customConfig.uploadImgMaxSize = 2 * 1024 * 1024; // 将图片大小限制为 2M
       this.editor.customConfig.uploadImgMaxLength = 6; // 限制一次最多上传 6 张图片
       this.editor.customConfig.uploadImgTimeout = 3 * 60 * 1000; // 设置超时时间
 
       // 配置菜单
-      this.editor.customConfig.menus = [
-        "bold", // 粗体
-        "italic", // 斜体
-        "underline", // 下划线
-        "fontSize", // 字号
-        "fontName", // 字体
-        "strikeThrough", // 删除线
-        "foreColor", // 文字颜色
-        "backColor", // 背景颜色
-        "link", // 插入链接
-        "list", // 列表
-        "justify", // 对齐方式
-        "quote", // 引用
-        "emoticon", // 表情
-        "image", // 插入图片
-        "table", // 表格
-        "video", // 插入视频
-        "code", // 插入代码
-        "fullscreen" // 全屏
-      ];
+      this.editor.customConfig.menus = this.configMenus;
 
       this.editor.customConfig.uploadImgHooks = {
         fail: (xhr, editor, result) => {
@@ -121,20 +127,20 @@ export default {
             this.$Message.error(result.msg);
           }
           // }
-        }
+        },
       };
-      this.editor.customConfig.onchange = html => {
+      this.editor.customConfig.onchange = (html) => {
         this.info_ = html; // 绑定当前逐渐地值
         this.$emit("update:value", this.info_); // 将内容同步到父组件中
       };
-      this.editor.customConfig.onblur = html => {
+      this.editor.customConfig.onblur = (html) => {
         this.info_ = html; // 绑定当前逐渐地值
         this.$emit("update:value", this.info_);
       };
       // 创建富文本编辑器
       this.editor.create();
-    }
-  }
+    },
+  },
 };
 </script>
 
