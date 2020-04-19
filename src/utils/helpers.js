cdb22ea9d8f5093fe925d4db16662a9af267acc9
Cookie726 <2544967914@qq.com>
2020-04-19 19:05:06 +0800

    新增 isComplete 工具函数

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,3 +5,17 @@ export const initAnswerType = (type) => {
         return []
     }
 }
+
+export const isComplete = (type, newVal) => {
+    if (!newVal) return false
+    let val
+    if (type === '简答题') {
+        const reg = /<[^<>]+>/g
+        val = newVal.replace(reg, "")
+    } else if (type === '单选题' || type === '判断题') {
+        val = newVal
+    } else if (type === '多选题') {
+        val = newVal.join("")
+    }
+    return val !== ""
+}
